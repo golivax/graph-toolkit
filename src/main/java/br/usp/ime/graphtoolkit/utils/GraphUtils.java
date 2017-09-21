1ed9373a14598f03c8a8753caac353ca3a16a540
Gustavo Oliva <golivax@gmail.com>
2017-09-21 15:11:59 -0400

    Added methods to compute the union of two graphs

--- src/main/java/br/usp/ime/graphtoolkit/utils/GraphUtils.java
+++ src/main/java/br/usp/ime/graphtoolkit/utils/GraphUtils.java
@@ -1,6 +1,9 @@
 package br.usp.ime.graphtoolkit.utils;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import edu.uci.ics.jung.graph.DirectedGraph;
+import edu.uci.ics.jung.graph.DirectedSparseGraph;
 import edu.uci.ics.jung.graph.UndirectedGraph;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
@@ -17,11 +20,11 @@ public class GraphUtils {
 
 		UndirectedGraph<V,E> out = new UndirectedSparseGraph<>();
 
-		for (V v : directedGraph.getVertices())
+		for (V v : directedGraph.getVertices()) {
 			out.addVertex(v);
+		}
 
-		for (E e : directedGraph.getEdges())
-		{
+		for (E e : directedGraph.getEdges()) {
 			Pair<V> endpoints = directedGraph.getEndpoints(e);
 			V v1 = endpoints.getFirst();
 			V v2 = endpoints.getSecond();
@@ -29,5 +32,33 @@ public class GraphUtils {
 			out.addEdge(to_add, v1, v2, EdgeType.UNDIRECTED);
 		}
 		return out;
+	}
+
+	public static <V,E> DirectedGraph<V,E> union(DirectedGraph<V,E> g1, DirectedGraph<V,E> g2) {
+		DirectedGraph<V,E> unionGraph = new DirectedSparseGraph<>();
+		for (V v : CollectionUtils.union(g1.getVertices(), g2.getVertices())) {
+			unionGraph.addVertex(v);
+		}
+		for (E e : g1.getEdges()) {
+			unionGraph.addEdge(e, g1.getSource(e), g1.getDest(e));
+		}
+		for (E e : g2.getEdges()) {
+			unionGraph.addEdge(e, g2.getSource(e), g2.getDest(e));
+		}
+		return unionGraph;
+	}
+	
+	public static <V,E> UndirectedGraph<V,E> union(UndirectedGraph<V,E> g1, UndirectedGraph<V,E> g2) {
+		UndirectedGraph<V,E> unionGraph = new UndirectedSparseGraph<>();
+		for (V v : CollectionUtils.union(g1.getVertices(), g2.getVertices())) {
+			unionGraph.addVertex(v);
+		}
+		for (E e : g1.getEdges()) {
+			unionGraph.addEdge(e, g1.getEndpoints(e));
+		}
+		for (E e : g2.getEdges()) {
+			unionGraph.addEdge(e, g2.getEndpoints(e));
+		}
+		return unionGraph;
 	}
 }
